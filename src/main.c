f35832bf9b208e707c353344243111f6a7e4766a
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-05-20 16:30:29 +0200

    Added toPdf option in command

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 // ----- Vars -----
 HashTableList *hashTableList;
@@ -11,6 +12,8 @@ FunctionHashTable *functionHashTable;
 int *nodeIndex;
 char *inputfile;
 char *outputFile;
+char cwd[255];
+int topdf = 0;
 FunctionError *functionError;
 
 // ----- lex/yacc -----
@@ -21,6 +24,10 @@ void yyerror(char const *s);
 // ----- Utils -----
 int parseOperation(int a, int b, char *op);
 void parseArgs(int argc, char **argv);
+void printfHelpMessage();
+void createError(char *error);
+void createFunctionError(char *error, int lineno, int column);
+void outputToPdf();
 
 
 int main(int argc, char **argv) {
@@ -43,13 +50,45 @@ int main(int argc, char **argv) {
     yyparse();
 
     printf("\033[1;32mCompiled successfully!\033[0m\n");
+
+
+    if (topdf == 1) outputToPdf();
+
     return 0;
 }
 
 void printfHelpMessage() {
-    printf("Usage: ./minigcc <input file> [output file]\n");
+    printf("Usage: ./minigcc [options] <input file> [output file]\n\n");
+    printf("\tOptions:\n");
+    printf("\t\t-h, --help\t\tPrints this help message\n");
+    printf("\t\t-v, --version\t\tPrints the version\n");
+    printf("\t\t-toPdf\t\t\tConverts the output file to pdf\n\n");
     printf("\tOutput file defaults to output.dot\n");
     printf("\tExample: ./minigcc test.c out.dot\n");
+}
+
+void outputToPdf() {
+    // ----- Output to pdf -----
+    printf("\033[1;32mOutputing to pdf\033[0m\n");
+
+    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+        printf("\033[1;31mError: could not get current working directory\033[0m\n");
+        exit(1);
+    }
+
+    char *pdfOutputFile = malloc(sizeof(char) * (strlen(outputFile) + 5));
+    strcpy(pdfOutputFile, outputFile);
+    strtok(pdfOutputFile, ".");
+    strcat(pdfOutputFile, ".pdf");
+
+    char *command = malloc(sizeof(char) * (strlen(outputFile) + 100));
+    sprintf(command, "dot -Tpdf %s -o %s", outputFile, pdfOutputFile);
+    if (system(command) == -1) {
+        printf("\033[1;31mError: could not convert to pdf\033[0m\n");
+        exit(1);
+    }
+
+    printf("\033[1;32mOutputed to %s/%s\033[0m\n", cwd, pdfOutputFile);
 }
 
 void parseArgs(int argc, char **argv) {
@@ -59,16 +98,20 @@ void parseArgs(int argc, char **argv) {
         exit(1);
     }
 
-    if (argc > 1) {
+    int i = 1;
+    if (argc > i) {
+        if (argv[1][0] == '-') i++;
         if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
             printfHelpMessage();
             exit(0);
         } else if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0) {
             printf("minigcc version 1.0.0\n");
             exit(0);
+        } else if (strcmp(argv[1], "-toPdf") == 0) {
+            topdf = 1;
         }
 
-        FILE *file = fopen(argv[1], "r");
+        FILE *file = fopen(argv[i], "r");
         if (!file) {
             printf("\033[1;31mError: file not found\033[0m\n");
             exit(1);
@@ -77,7 +120,7 @@ void parseArgs(int argc, char **argv) {
         yyin = file;
     }
 
-    if (argc > 2) outputFile = argv[2];
+    if (argc > i + 1) outputFile = argv[i + 1];
     else
         outputFile = "output.dot";
 
